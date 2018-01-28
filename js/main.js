0349005f0615f27a33311598e2b302ff2813a591
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-01-28 11:52:26 -0700

    To be reverted

--- js/main.js
+++ js/main.js
@@ -273,5 +273,68 @@ $(function(){
     });
 });
 
-// Home Screen Vector Animation
+// Scroll to Top
 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {        
+        $('#return-to-top').fadeIn(200);    
+    } else {
+        $('#return-to-top').fadeOut(200);   
+    }
+});
+
+$('#return-to-top').click(function() {      
+    $('body,html').animate({
+        scrollTop : 0                       
+    }, 500);
+});
+
+// Scroll Indicator
+
+jQuery(function($){
+	var growmouseover = [true, '25px'] 
+	var $indicatorparts = $(document.body).append('<div class="scrollindicator"><div class="scrollprogress"></div></div>')
+	var $indicatorMain = $indicatorparts.find('div.scrollindicator')
+	var $scrollProgress = $indicatorparts.find('div.scrollprogress')
+	var indicatorHeight = $indicatorMain.outerHeight()
+	var transformsupport = $scrollProgress.css('transform')
+	transformsupport = (transformsupport == "none" || transformsupport =="")? false: true
+
+	function syncscrollprogress(){
+	    var winheight = $(window).height()
+	    var docheight = $(document).height()
+	    var scrollTop = $(window).scrollTop()
+	    var trackLength = docheight - winheight
+	    var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 NaN if tracklength == 0)
+			$scrollProgress.css('transform', 'translate3d(' + (-100 + pctScrolled) + '%,0,0)')
+	}
+	
+	if (transformsupport){
+		$indicatorMain.css('visibility', 'visible')
+	
+		$indicatorMain.on('click', function(e){
+			var trackLength = $(document).height() - $(window).height()
+			var scrollamt = e.clientX/($(window).width()-32) * trackLength
+			$('html,body').animate({scrollTop: scrollamt}, 'fast')
+		})
+	
+		if (growmouseover[0]){
+			$indicatorMain.on('mouseenter touchstart', function(e){
+				$(this).css('height', growmouseover[1])
+				e.stopPropagation()
+			})
+		
+			$indicatorMain.on('mouseleave', function(e){
+				$(this).css('height', indicatorHeight)
+			})
+			
+			$(document).on('touchstart', function(e){
+				$indicatorMain.css('height', indicatorHeight)
+			})
+		}
+		
+		$(window).on("scroll load", function(){
+			requestAnimationFrame(syncscrollprogress)
+		})
+	}
+})
