4c004f633003d98caa3f38bc977931007f6b15d5
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-01-30 00:11:33 -0700

    Competitive activities section

--- js/main.js
+++ js/main.js
@@ -575,3 +575,106 @@ jQuery(document).ready(function($){
 		}
 	});
 });
+
+// Competitive Activities Slider
+
+(function($){
+$(function(){
+  $('.before-wrapper').on( "mousemove", function(e) {
+    var offsets = $(this).offset();
+    var fullWidth = $(this).width();
+    var mouseX = e.pageX - offsets.left;
+
+    if (mouseX < 0) { mouseX = 0; }
+    else if (mouseX > fullWidth) { mouseX = fullWidth }
+
+
+    $(this).parent().find('.comparison-slider').css({
+      left: mouseX,
+      transition: 'none'
+    });
+    $(this).find('.after-wrapper').css({
+      transform: 'translateX(' + (mouseX) + 'px)',
+      transition: 'none'
+    });
+    $(this).find('.after-image').css({
+      transform: 'translateX(' + (-1*mouseX) + 'px)',
+      transition: 'none'
+    });
+  });
+  $('.slider-wrapper').on( "mouseleave", function() {
+    $(this).parent().find('.comparison-slider').css({
+      left: '50%',
+      transition: 'all .3s'
+    });
+    $(this).find('.after-wrapper').css({
+      transform: 'translateX(50%)',
+      transition: 'all .3s'
+    });
+    $(this).find('.after-image').css({
+      transform: 'translateX(-50%)',
+      transition: 'all .3s'
+    });
+  });
+
+}); 
+})(jQuery); 
+
+// Projects Slider
+
+$(document).ready(function(event){
+	var projectsContainer = $('.cd-projects-container'),
+		navigation = $('.cd-primary-nav'),
+		triggerNav = $('.cd-nav-trigger'),
+		logo = $('.cd-logo');
+	
+	triggerNav.on('click', function(){
+		if( triggerNav.hasClass('project-open') ) {
+			//close project
+			projectsContainer.removeClass('project-open').find('.selected').removeClass('selected').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$(this).children('.cd-project-info').scrollTop(0).removeClass('has-boxshadow');
+
+			});
+			triggerNav.add(logo).removeClass('project-open');
+		} else {
+			//trigger navigation visibility
+			triggerNav.add(projectsContainer).add(navigation).toggleClass('nav-open');
+		}
+	});
+
+	projectsContainer.on('click', '.single-project', function(){
+		var selectedProject = $(this);
+		if( projectsContainer.hasClass('nav-open') ) {
+			//close navigation
+			triggerNav.add(projectsContainer).add(navigation).removeClass('nav-open');
+		} else {
+			//open project
+			selectedProject.addClass('selected');
+			projectsContainer.add(triggerNav).add(logo).addClass('project-open');
+		}
+	});
+
+	projectsContainer.on('click', '.cd-scroll', function(){
+		//scroll down when clicking on the .cd-scroll arrow
+		var visibleProjectContent =  projectsContainer.find('.selected').children('.cd-project-info'),
+			windowHeight = $(window).height();
+
+		visibleProjectContent.animate({'scrollTop': windowHeight}, 300); 
+	});
+
+	//add/remove the .has-boxshadow to the project content while scrolling 
+	var scrolling = false;
+	projectsContainer.find('.cd-project-info').on('scroll', function(){
+		if( !scrolling ) {
+		 	(!window.requestAnimationFrame) ? setTimeout(updateProjectContent, 300) : window.requestAnimationFrame(updateProjectContent);
+		 	scrolling = true;
+		}
+	});
+
+	function updateProjectContent() {
+		var visibleProject = projectsContainer.find('.selected').children('.cd-project-info'),
+			scrollTop = visibleProject.scrollTop();
+		( scrollTop > 0 ) ? visibleProject.addClass('has-boxshadow') : visibleProject.removeClass('has-boxshadow');
+		scrolling = false;
+	}
+});
