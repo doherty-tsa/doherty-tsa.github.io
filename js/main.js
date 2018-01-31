c571790640a523e2242391b1920cc2d0d34f6e70
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-01-30 23:34:09 -0700

    Vimeo embed

--- js/main.js
+++ js/main.js
@@ -833,3 +833,41 @@ jQuery(document).ready(function($){
 		});
 	}
 });
+
+// Video Player
+
+var myPlayer,
+  playlistIds = ["4450721964001", "2805100167001", "2764931905001","1754200320001"],
+  playlistIdsLength = playlistIds.length,
+  tabs = document.getElementsByClassName("button"),
+  currentTab,
+  currentTabName,
+  playlistNames = [];
+
+videojs("video_1").ready(function() {
+  myPlayer = this;
+  // console.log("ready");
+  processTab(0);
+});
+
+function processTab(index) {
+  // console.log("processTab");
+  resetTabs();
+  document.getElementById("tab" + index).setAttribute("style", "background:#202020;color: #fff; border-bottom: 3px solid #84A9B7;");
+  loadPlaylist(playlistIds[index]);
+};
+
+function loadPlaylist(currentId) {
+  myPlayer.catalog.getPlaylist(currentId, function(error, playlist) {
+    myPlayer.catalog.load(playlist);
+    myPlayer.playlist.currentItem (0);
+  });
+};
+
+function resetTabs() {
+  var i,
+    iMax = tabs.length;
+  for (i = 0; i < iMax; i++) {
+    tabs[i].setAttribute("style", "background: #0000cc;color: #fff;")
+  }
+}
