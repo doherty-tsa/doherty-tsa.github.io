f14891093e05261551fa18d647031b70f549a9e1
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-01-31 20:17:18 -0700

    Interview section, start on design brief

--- js/main.js
+++ js/main.js
@@ -836,38 +836,18 @@ jQuery(document).ready(function($){
 
 // Video Player
 
-var myPlayer,
-  playlistIds = ["4450721964001", "2805100167001", "2764931905001","1754200320001"],
-  playlistIdsLength = playlistIds.length,
-  tabs = document.getElementsByClassName("button"),
-  currentTab,
-  currentTabName,
-  playlistNames = [];
-
-videojs("video_1").ready(function() {
-  myPlayer = this;
-  // console.log("ready");
-  processTab(0);
-});
-
-function processTab(index) {
-  // console.log("processTab");
-  resetTabs();
-  document.getElementById("tab" + index).setAttribute("style", "background:#202020;color: #fff; border-bottom: 3px solid #84A9B7;");
-  loadPlaylist(playlistIds[index]);
-};
-
-function loadPlaylist(currentId) {
-  myPlayer.catalog.getPlaylist(currentId, function(error, playlist) {
-    myPlayer.catalog.load(playlist);
-    myPlayer.playlist.currentItem (0);
+/*JS FOR SCROLLING THE ROW OF THUMBNAILS*/ 
+$(document).ready(function () {
+  $('.vid-item').each(function(index){
+    $(this).on('click', function(){
+      var current_index = index+1;
+      $('.vid-list li').removeClass('active');
+      $('.vid-list li:nth-child('+current_index+') .thumb').addClass('active');
+    });
   });
-};
-
-function resetTabs() {
-  var i,
-    iMax = tabs.length;
-  for (i = 0; i < iMax; i++) {
-    tabs[i].setAttribute("style", "background: #0000cc;color: #fff;")
-  }
-}
+  var $list = $('#vid-list li');
+  $list.click(function(){
+    $list.removeClass('selected');
+    $(this).addClass('selected'); 
+  });
+});
