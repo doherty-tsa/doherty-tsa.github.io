5b805326407b76872e6f2d27ff76d73e420910f5
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-02-18 00:21:56 -0700

    Adjusted color scheme & loading screen animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// Pre-load animation
+
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#status').fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(1350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(1350).css({'overflow':'visible'});
+})
+
 
 // Navigation
 
