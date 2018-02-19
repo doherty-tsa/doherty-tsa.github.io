5d898b80505ee67934091bf3841f9d0aac61cef2
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-02-19 15:59:55 -0700

    Future plans transition adjustments

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 
 $(window).on('load', function() { // makes sure the whole site is loaded 
   $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(1350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(1350).css({'overflow':'visible'});
+  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(350).css({'overflow':'visible'});
 })
 
 
